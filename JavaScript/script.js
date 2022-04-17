53312e6233cf8af628eda58d6b6f28dd2cadd936
Luiz Gustavo Pereira Schroeder <85582007+Luizgps@users.noreply.github.com>
2022-04-17 18:49:45 -0300

    Update
    - Botões com alertbox;
    - Botão abre localização Growth;
    - Botão abre site Growth;
    - Comentado CSS;
    - Témino comentarios HTML;
    - Feito JavaScript para chamar botões e abrir guias;
    - Removido barra X;

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+let logoOpen = document.getElementById('logo');
+logoOpen.addEventListener('click', () => {
+    window.open('https://growthhouse.com.br', '_blank');
+})//CLICK LOGO = ABRIR SITE GROWTH HOUSE
+let btnOpen = document.getElementById('button');
+btnOpen.addEventListener('click', () => {
+    window.open('https://growthhouse.com.br', '_blank');
+})//CLICK BOTAO = ABRIR SITE GROWTH HOUSE
+let mapsOpen = document.getElementById('maps');
+mapsOpen.addEventListener('click', () => {
+    window.open('https://www.google.com/maps/place/Growth+House/@-23.5595883,-46.658066,15z/data=!4m2!3m1!1s0x0:0x528c771b2263bb05?sa=X&ved=2ahUKEwj1yJHr-pv3AhVYvZUCHYaqAj8Q_BJ6BAhSEAU', '_blank');
+})//CLICK PING = ABRIR LOCALIZACAO GROTH HOUSE
